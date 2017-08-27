81be0dd4fe1ccfed529656b68f84ff2eb475d1a6
AlexandrShestak <shestakalexandr@mail.ru>
2017-08-27 13:57:16 +0300

    Added 'Two TV' solution

---
+++ src/com/shestakam/twoTv/Solution.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        List<Range> ranges = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            int from = sc.nextInt();
+            int to = sc.nextInt();
+            ranges.add(new Range(from, to));
+        }
+        ranges.sort(Comparator.comparing(o -> o.from));
+
+        int firstTvStart = -1;
+        int secondTvStart = -1;
+        for (Range range : ranges) {
+            if (firstTvStart < range.from) {
+                firstTvStart = range.to;
+            } else if (secondTvStart < range.from) {
+                secondTvStart = range.to;
+            } else {
+                System.out.println("NO");
+                return;
+            }
+        }
+        System.out.println("YES");
+    }
+
+    static class Range {
+        Integer from;
+        Integer to;
+
+        public Range(int from, int to) {
+            this.from = from;
+            this.to = to;
+        }
+    }
+}
